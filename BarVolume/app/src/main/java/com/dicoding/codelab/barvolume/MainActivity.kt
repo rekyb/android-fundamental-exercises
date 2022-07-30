2e7c203e3e5b8f9afd38a2818661ec790fc12d4f
Reky Bongso <rekybongso@gmail.com>
2022-07-31 07:04:03 +0800

    Add IntentApp codelab

--- BarVolume/app/src/main/java/com/dicoding/codelab/barvolume/MainActivity.kt
+++ BarVolume/app/src/main/java/com/dicoding/codelab/barvolume/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dicoding.codelab.barvolume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
---
+++ IntentApp/app/src/main/java/com/dicoding/codelab/intentapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dicoding.codelab.intentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnMoveActivity: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+}
