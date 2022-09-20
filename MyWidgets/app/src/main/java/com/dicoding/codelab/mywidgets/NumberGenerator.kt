255056b64734fdd15bfb17e0044a9ce310b875e8
Reky Bongso <rekybongso@gmail.com>
2022-09-20 11:38:29 +0800

    Add widget

---
+++ MyWidgets/app/src/main/java/com/dicoding/codelab/mywidgets/NumberGenerator.kt
@@ -0,0 +1,10 @@
+package com.dicoding.codelab.mywidgets
+
+import java.util.Random
+
+internal object NumberGenerator {
+    fun generate(max: Int): Int {
+        val random = Random()
+        return random.nextInt(max)
+    }
+}
---
+++ MyWidgets/app/src/main/java/com/dicoding/codelab/mywidgets/RandomNumberWidget.kt
@@ -0,0 +1,88 @@
+package com.dicoding.codelab.mywidgets
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+
+@Suppress("SameParameterValue")
+class RandomNumberWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val WIDGET_CLICK = "android.appwidget.action.APPWIDGET_UPDATE"
+        private const val WIDGET_ID_EXTRA = "widget_id_extra"
+    }
+
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val lastUpdate = "Random: " + NumberGenerator.generate(100)
+        val remoteViews = RemoteViews(context.packageName, R.layout.random_number_widget)
+
+        remoteViews.setTextViewText(R.id.appwidget_text, lastUpdate)
+        remoteViews.setOnClickPendingIntent(
+            R.id.btn_click,
+            getPendingSelfIntent(context, appWidgetId, WIDGET_CLICK)
+        )
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        if (WIDGET_CLICK == intent.action) {
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val remoteViews = RemoteViews(context.packageName, R.layout.random_number_widget)
+            val lastUpdate = "Random: " + NumberGenerator.generate(100)
+            val appWidgetId = intent.getIntExtra(WIDGET_ID_EXTRA, 0)
+
+            remoteViews.setTextViewText(R.id.appwidget_text, lastUpdate)
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        }
+    }
+
+    private fun getPendingSelfIntent(
+        context: Context,
+        appWidgetId: Int,
+        action: String
+    ): PendingIntent {
+        val intent = Intent(context, RandomNumberWidget::class.java)
+        intent.action = action
+        intent.putExtra(WIDGET_ID_EXTRA, appWidgetId)
+        return PendingIntent.getActivity(
+            context,
+            appWidgetId,
+            intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+            } else {
+                0
+            }
+        )
+    }
+}
+
+
